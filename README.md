b89eccb1bde8781912434cd2cced89e5df8acc27
JanPalasek <jan.palasek@seznam.cz>
2022-07-10 11:25:26 +0200

    Init commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pretty Jupyter Examples
+
+Shows, how Pretty Jupyter can be used by using examples.
---
+++ extending_template/custom_preprocessor.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from nbconvert.preprocessors import Preprocessor
+
+class CustomPreprocessor(Preprocessor):
+    def preprocess(self, nb, resources):
+        resources["date"] = datetime.now()
+        return super().preprocess(nb, resources)
+
+    def preprocess_cell(self, cell, resources, index):
+        return cell, resources
